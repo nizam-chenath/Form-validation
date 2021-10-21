5a013b68538f287a08359c86df6eb4d2ae5aa5ad
nizam-chenath <nizamstudymajliz@gmail.com>
2021-10-21 22:06:27 +0530

    form validation completed

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 function clk(){
-    var uname = document.getElementById("trxt");
-    var pwd = document.getElementById("password");
+    var uname = document.getElementById("text");
+    var pwd = document.getElementById("pass");
 
-    if(uname.value == ''){
-        alert("fill in the blanks")
+    if(uname.value.trim() == ''){
+        alert("fill in the blank username");
+        uname.style.border = "solid 3px red";
+        document.getElementById("valied").style.visibility = "visible";
         return false;
     }
+    else if (pwd.value.trim() == ''){
+        pwd.style.border = "solid 3px red";
+        document.getElementById("invalied").style.visibility = "visible";
+        alert(" fill in the password")
+        return false;
+    }
+    else if (pwd.value.trim().length<5){
+        pwd.style.border = "solid 3px red";
+        alert(" fill in the password atleast 6 character")
+        return false;
+    }
+
     else{
-        true;
+        return true;
     }
 }
