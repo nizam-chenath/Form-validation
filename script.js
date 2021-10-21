1acdb0b21e7786ef0874ba5b782b760841470369
nizam-chenath <nizamstudymajliz@gmail.com>
2021-10-21 14:01:00 +0530

    form

---
+++ script.js
@@ -0,0 +1,12 @@
+function clk(){
+    var uname = document.getElementById("trxt");
+    var pwd = document.getElementById("password");
+
+    if(uname.value == ''){
+        alert("fill in the blanks")
+        return false;
+    }
+    else{
+        true;
+    }
+}
